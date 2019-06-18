f49d88d27cb5a5e1c84f5233ea8bcaa65e32c80c
Ciprian Rosculescu <ciprian.rosculescu@endava.com>
2019-06-18 11:26:45 +0300

    augmented rest caller

--- src/test/common/functionality/calls/MakeRestCall.ts
+++ src/test/common/functionality/calls/MakeRestCall.ts
@@ -6,4 +6,19 @@ export class RestCaller {
         this.axiosRespone = await axios.get(uri);
         return this.axiosRespone;
     }
+
+    get = async uri  => {
+        this.axiosRespone = await axios.get(uri);
+        return this.axiosRespone;
+    }
+
+    post = async uri => {
+        this.axiosRespone = await axios.post(uri);
+        return this.axiosRespone;
+    }
+
+    put = async uri => {
+        this.axiosRespone = await axios.put(uri);
+        return this.axiosRespone;
+    } 
 }
